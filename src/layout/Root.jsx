7918627acd0318396c717de7d9ed5e002fb2e173
rfaraf <mohsinarfat53@gamil.com>
2024-08-15 16:24:29 +0600

    add: navbar and footer

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
 
 const Root = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <Navbar></Navbar>
+      <div className="px-5 lg:max-w-4xl xl:max-w-6xl mx-auto min-h-[calc(100vh-117px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
