f9970e117eac4547c576476654a506dd5a4ce245
rfaraf <mohsinarfat53@gamil.com>
2024-08-16 18:00:08 +0600

    add: bookCard

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,36 @@
+import { FaStar } from "react-icons/fa";
+
+const BookCard = ({ book }) => {
+  const {
+    bookName,
+    bookImage,
+    description,
+    price,
+    category,
+    ratings,
+    bookCreationDate,
+  } = book;
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure className="p-5 bg-gray-100">
+        <img src={bookImage} className="w-2/3 h-[300px] rounded-md" />
+      </figure>
+      <div className="card-body p-4 items-center text-center">
+        <h2 className="card-title">{bookName}</h2>
+        <p>{description}</p>
+        <div className="font-medium">
+          <span>{category}</span>
+          <span className="mx-2 font-bold">|</span>
+          <span>
+            {ratings} <FaStar />
+          </span>
+          <span className="mx-2 font-bold">|</span>
+          <span>{bookCreationDate}</span>
+        </div>
+        <h2 className="text-2xl font-medium">${price}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -1,6 +1,6 @@
-import { FaStar } from "react-icons/fa";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
+import BookCard from "../../components/bookCard";
 
 const Books = () => {
   const axiosPublic = useAxiosPublic();
@@ -19,33 +19,16 @@ const Books = () => {
 
   console.log(books);
 
+  books.map((book) => console.log(book));
+
   return (
     <div className="my-10">
       <h1>books are here...</h1>
 
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="p-5 bg-gray-100">
-          <img
-            src="https://i.ibb.co/0y8yMqW/A-Culture-of-Promise.jpg"
-            className="w-2/3 rounded-md"
-          />
-        </figure>
-        <div className="card-body p-4 items-center text-center">
-          <h2 className="card-title">A Culture of Promise</h2>
-          <p>
-            Explores the foundations of building a culture rooted in trust,
-            transparency, and promise.
-          </p>
-          <div className="font-medium">
-            <span>Non-Fiction</span> <span className="mx-2 font-bold">|</span>{" "}
-            <span>4.5</span> <FaStar />{" "}
-            <span className="mx-2 font-bold">|</span> <span>01/08/2024</span>
-          </div>
-          <h2 className="text-2xl font-medium">$18.99</h2>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
+      <div className="grid grid-cols-3 gap-6">
+        {books.map((book) => (
+          <BookCard key={book?._id} book={book}></BookCard>
+        ))}
       </div>
     </div>
   );
